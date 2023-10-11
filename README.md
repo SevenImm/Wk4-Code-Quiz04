1dc09e5145df2916deada685809465446d79588b
SevenImm <angelcardenasptx@gmail.com>
2023-10-10 20:22:09 -0500

    Landing page now disappears when click button

--- README.md
+++ README.md
@@ -5,6 +5,11 @@ When I open the website page am greated,
 Give instructions on how the quiz works,
 Add a button that starts the quiz.
 
+## Feature/StartHide
+For this branch I want to add an event listener to the start button so that
+- When I press the start button
+- The landing page disappears
+
 
 # Screenshots:
 ## Feature/start:
--- asset/script.js
+++ asset/script.js
@@ -1,2 +1,12 @@
-let starQuiz = document.getElementById('start-Quiz')
+document.addEventListener("DOMContentLoaded", function() {
+// Select the start quiz button by its ID
+let startButton = document.querySelector('.start-Quiz');
+// Select the landing page by its ID
+let landingPage = document.querySelector('.landing-Page');
 
+startButton.addEventListener('click', function() {
+    landingPage.style.display = 'none'
+    console.log('clicked button. Landing page hidden.');
+});
+
+});
