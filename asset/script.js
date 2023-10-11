4dfee4962f0779c396f6a51860d018d90ce5a2a8
SevenImm <angelcardenasptx@gmail.com>
2023-10-10 22:15:25 -0500

    Function successfully displays first element of the array

--- asset/script.js
+++ asset/script.js
@@ -1,12 +1,34 @@
-document.addEventListener("DOMContentLoaded", function() {
-// Select the start quiz button by its ID
-let startButton = document.querySelector('.start-Quiz');
-// Select the landing page by its ID
-let landingPage = document.querySelector('.landing-Page');
+document.addEventListener("DOMContentLoaded", function () {
+    // Select the start quiz button by its class
+    let startButton = document.querySelector('.start-Quiz');
+    // Select the landing page by its class
+    let landingPage = document.querySelector('.landing-Page');
+    // Target the question div and remove the class attribute
+    let questionPage = document.getElementById('question-Page');
 
-startButton.addEventListener('click', function() {
-    landingPage.style.display = 'none'
-    console.log('clicked button. Landing page hidden.');
-});
+    startButton.addEventListener('click', function () {
+        landingPage.style.display = 'none'
+        console.log('clicked button. Landing page hidden.');
+        questionPage.removeAttribute('class');
+        displayQuestion();
+    });
+    // Start the object index always at 0
+    let startQuizIndex = 0;
+    // Start display question function:
+    // attribute the title object to the h2 title
+    // attribute the option object to the options div
+    function displayQuestion() {
+        let startQuiz = questions[startQuizIndex];
+        // Target the element title & options
+        let questionTitle = document.getElementById('question');
+        let questionOptions = document.getElementById('options');
 
+        // display the question title
+        questionTitle.textContent = startQuiz.title;
+
+        // clear the previous options
+        questionOptions.innerHTML = '';
+        
+
+    }
 });
